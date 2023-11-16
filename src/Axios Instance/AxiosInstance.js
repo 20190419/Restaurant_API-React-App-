5ef1c1017802063fb96198ece956af28913e11b0
Mohamed Eslam Amin <60846785+20190419@users.noreply.github.com>
2023-11-16 14:49:38 +0200

    Update AxiosInstance.js

--- src/Axios Instance/AxiosInstance.js
+++ src/Axios Instance/AxiosInstance.js
@@ -1,5 +1,10 @@
+// Import the Axios library
 import axios from "axios";
+
+// Create an Axios instance with a specific base URL
 const instance = axios.create({
   baseURL: "https://swensonhe-dev-challenge.s3.us-west-2.amazonaws.com/",
 });
+
+// Export the configured Axios instance as the default export of this module
 export default instance;
