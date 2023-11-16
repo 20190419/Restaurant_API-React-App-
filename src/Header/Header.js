098dcce921218d7e121cae9930cf407d047c9e78
Mohamed Eslam Amin <60846785+20190419@users.noreply.github.com>
2023-11-16 14:50:33 +0200

    Update Header.js

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,37 +1,48 @@
+// Import necessary components and styles from external libraries and files
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Header.css";
+import "./Header.css"; // Import custom styles
 import { Image } from "react-bootstrap";
 
+// Define and export the Header component
 export default function Header() {
   return (
     <>
+      {/* Top navigation bar */}
       <Navbar expand="lg" className="tm-top-header">
+        {/* Container for the navigation bar */}
         <Container className="container">
+          {/* Brand/logo section with a link to the home page */}
           <Navbar.Brand
             as={Link}
             to="/"
-            // style={{
-            //   fontSize: "35px",
-            // }}
+            // Additional styles can be applied here if needed
             className="tm-logo-container"
           >
+            {/* Site logo */}
             <img
               src="/public/logo192.png"
               alt="logo"
               className="tm-site-logo"
             />
+            {/* Site name */}
             <h1 className="tm-site-name tm-handwriting-font">Store Name</h1>
           </Navbar.Brand>
+
+          {/* Hamburger menu icon for mobile view */}
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+          {/* Navigation links */}
           <Navbar.Collapse
             id="basic-navbar-nav"
             className="tm-top-header-inner"
           >
+            {/* Navigation links container */}
             <Nav className="tm-nav">
+              {/* Home link */}
               <Link
                 to="/"
                 className="nav-link"
@@ -39,6 +50,8 @@ export default function Header() {
               >
                 Home
               </Link>
+
+              {/* Products link */}
               <Link to="/categories" className="nav-link">
                 Products
               </Link>
